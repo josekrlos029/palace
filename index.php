3b4eba5775ce77ef2132a0abdf0180cdea87ddf3
JoseCarlos <JoseCarlos@JoseKrlos>
2014-04-27 00:19:16 -0500

    26/04/2014... primera subida

--- index.php
+++ index.php
@@ -1,12 +1,55 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+
+define('DS', DIRECTORY_SEPARATOR);
+define('HOME', dirname(__FILE__));
+
+
+ini_set('display_erros', 1);
+/**
+ * Esta funcion se utiliza para cambiar de INFORMACION DE ERRORES
+ * a MANEJO DE EXCEPCIONES
+ * @param type $code
+ * @param type $mensaje
+ * @param type $archivo
+ * @param type $linea
+ * @param type $contexto
+ * @return boolean
+ * @throws ErrorException
+ */
+function manejadorErrores($code, $mensaje, $archivo, $linea, $contexto = NULL){
+    if(E_RECOVERABLE_ERROR === $code){
+        throw new ErrorException($mensaje, $code,1, $archivo, $linea, NULL );
+    }
+    return false;
+}
+set_error_handler('manejadorErrores'); //Indica la funcion (callback) que manejara los errores
+                                       //Esto es para cambiar de manejo de errores a manejo de 
+                                       //Excepciones
+
+function cargadorClases(){
+    require_once './config/Configuracion.php';
+    require_once './config/Db.php';
+    require_once './modelo/Modelo.php';
+    require_once './modelo/Persona.php';
+    require_once './modelo/Evento.php';
+    require_once './modelo/Comentario.php';
+    require_once './modelo/Noticia.php';
+    require_once './modelo/Lugar.php';
+    require_once './modelo/Proyecto.php';
+    require_once './modelo/TipoEvento.php';
+    require_once './modelo/Unidad.php';
+    require_once './modelo/Usuario.php';
+    require_once './modelo/Inicio.php';
+    require_once './modelo/Administrador.php';
+    require_once './controlador/Controlador.php';
+    require_once './controlador/InicioControl.php';
+    require_once './controlador/AdministradorControl.php';
+    require_once './controlador/UsuarioControl.php';
+    require_once './vista/Vista.php';
+    
+}
+
+spl_autoload_register('cargadorClases');
+
+require_once './utiles/inicio.php';
+?>
