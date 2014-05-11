a7e6a3c49f42d229296e156bf33b856e06a74249
JoseCarlos <JoseCarlos@JoseKrlos>
2014-05-11 04:45:31 -0500

    Guarda Pedido

--- config/palace (4).sql
+++ config/palace (4).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-05-2014 a las 23:48:27
+-- Tiempo de generación: 11-05-2014 a las 11:44:01
 -- Versión del servidor: 5.6.16
 -- Versión de PHP: 5.5.9
 
@@ -94,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `ingreso_producto` (
   `precioFabrica` int(11) NOT NULL,
   PRIMARY KEY (`idIngreso`),
   KEY `idProducto_fk_ip_idx` (`idProducto`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Volcado de datos para la tabla `ingreso_producto`
@@ -105,7 +105,8 @@ INSERT INTO `ingreso_producto` (`idIngreso`, `idProducto`, `cantidad`, `fechaIng
 (2, 200, 300, '2014-04-05', 200),
 (3, 300, 400, '2014-04-05', 400),
 (4, 400, 100, '2014-04-05', 100),
-(5, 500, 450, '2014-04-05', 450);
+(5, 500, 450, '2014-04-05', 450),
+(6, 507, 500, '2014-05-11', 200);
 
 -- --------------------------------------------------------
 
@@ -138,12 +139,29 @@ INSERT INTO `persona` (`idPersona`, `nombres`, `pApellido`, `sApellido`, `sexo`,
 ('123', 'kju', 'uiulu', 'liulu', 'M', '0000-00-00', '21321', '123', 'dasdj', 'kdsaj@dsadl.com'),
 ('1234', 'kju', 'uiulu', 'liulu', 'M', '2014-12-31', '21321', '123', 'dasdj', 'kdsaj@dsadll.com'),
 ('123456', 'Jose Carlos ', 'Jimenez', 'Montenegro', 'M', '1005-02-12', '234234343', '11212339204', 'sadsakjlew', 'sdlksjaldksa@live.com'),
+('12345678', 'dalksdjl', 'jlsdajdl', 'jldsjaldj', 'M', '2014-12-31', '1233', '313', 'ldsajl', 'askljd@hotmail.com'),
+('123456789', 'jose', 'dakdj', 'kjdaskdj', 'M', '2014-12-31', '323', '3213', 'sdasd', 'dsakjdq@jakdasd.cm'),
+('1343241', 'ajsdasdj', 'dsajdjj', 'jsakjd', 'M', '2014-12-31', '123123', '12313', 'sadad', 'dasd@ad.com'),
 ('21334', 'jdsa', 'dsad', 'dasdas', 'M', '2014-12-31', '23123', '3213', 'dsakldk', 'dlasdklk@asdasd.com'),
 ('222', 'Andy', 'Bolaños', 'Castilla', 'M', '2014-04-08', '6578981234', '0987676', 'calle 222', 'sdkjfhsdf'),
+('234234', 'kdjalkdj', 'lkdjsaldkjl', 'kjdlaksjd', 'M', '2014-12-31', '123123', '3132', 'dsad', 'dadjqa@jdaskd.com'),
+('3123', 'sjadkjkj', 'kdsajdkj', 'kdjsakdj', 'M', '2014-12-31', '312313', '1233', 'kdsajkdj', 'dkajd@aldk.com'),
+('3124324', 'jaskdjkjdask', 'jdkdjkasjd', 'kjsakjd', 'M', '2014-12-31', '132123', '3123', 'dasd', 'dajkd@hotmail.com'),
+('321', 'ssdjkaj', 'kasdkajds', 'dkjkdajd', 'M', '2014-12-31', '21313', '123', 'lkdasldk', 'dasd@dsaldj.com'),
+('3213123', 'sadasdd', 'dsad', 'kdsada', 'M', '2014-12-31', '213123', '3213', 'dad', 'ddasd@dasl.com'),
+('324', 'dksadjk', 'jkdsajkjk', 'kdjksjdk', 'M', '2014-12-31', '313', '12313', 'sajdkjads', 'aksdjqd@dsakjd.com'),
 ('333', 'Jose ', 'Jimenez', 'Montenegro', 'M', '2014-04-06', '2309867', '8237456', 'calle 333', 'laskdfhkjads'),
+('34324', 'ldkad', 'lkdjalkdj', 'ldkjlj', 'M', '2014-12-31', '213123', '2131', 'asdasdkj', 'kadjkaqq@jksad.com'),
+('3871283', 'isaukdjkj', 'kjkdajkdj', 'kjdsakjd', 'M', '2014-01-01', '3213', '3213', 'kjdaskdj', 'kjdas@hotmail.com'),
+('4234234234', 'dasdkj', 'kajd', 'kjdaskdj', 'M', '2014-12-31', '12312', '3123', 'djkajd', 'kdjaskj@hotmail.com'),
+('4321', 'dad', 'dsad', 'dsad', 'M', '2014-12-31', '231', '1233', 'dasd', 'dsdad@asd.com'),
+('432421', 'kjdsakdj', 'kdjksajdk', 'jkdsajdk', 'M', '2014-12-31', '21312', '213', 'dasd', 'dasd@dakd.com'),
 ('444', 'Juan', 'Martinez', 'Oñate', 'M', '2014-04-04', '09328745', '4688797', 'Calle 444', 'wkegfihdfs'),
+('543231', 'dkjdkasjk', 'djskadjk', 'jdksjadkj', 'M', '2014-12-31', '3123', '1231', 'kajdk', 'jdaskdjq@jdadj.com'),
 ('555', 'Maria', 'Oñate', 'Fernandez', 'F', '2014-04-07', '346875', '67891234', 'calle 555', 'lasdhfkljdsaf'),
 ('777', 'jdahskdhj', 'kjdhsakdjh', 'kjhdaskdjh', 'M', '0000-00-00', '3123', '231', 'dkjd', 'dsa@kadk.com'),
+('823', 'jdkadj', 'kjdsakdj', 'kjdkasjd', 'M', '2014-12-31', '12312', '3213', 'ldakdl', 'jdado@dkad.com'),
+('831283', 'adhasjkdh', 'kjdhsakdhk', 'hdksajdkh', 'M', '2014-12-31', '3213', '3123', 'alsdkl', 'asldk@hotm.com'),
 ('87878978', 'ghjghjhj', 'ghjgjgjh', 'gjhjgjh', 'M', '2006-05-04', '678678687', '8979879', 'dfgdgf', 'dfdgfdg@ddd.com'),
 ('888', 'slkajdl', 'kjdslakjd', 'lkdjsald', 'M', '0000-00-00', '123123', '3213', 'dasd', 'dasd@dad.com'),
 ('999', 'kju', 'uiulu', 'liulu', 'M', '0000-00-00', '21321', '123', 'dasdj', 'kdsaj@dsad.com');
@@ -158,24 +176,27 @@ CREATE TABLE IF NOT EXISTS `producto` (
   `idProducto` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(60) NOT NULL,
   `precioVenta` int(11) NOT NULL,
+  `unidades` int(11) NOT NULL,
   PRIMARY KEY (`idProducto`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=506 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=508 ;
 
 --
 -- Volcado de datos para la tabla `producto`
 --
 
-INSERT INTO `producto` (`idProducto`, `nombre`, `precioVenta`) VALUES
-(100, 'Shampoo HyS', 500),
-(200, 'Acondicionador Loreal', 600),
-(300, 'Crema Pantene', 800),
-(400, 'Ampolla Loreal', 1500),
-(500, 'Gel Rolda', 1000),
-(501, 'Ampolla Sol', 500),
-(502, 'Ampolla X', 500),
-(503, 'ampolla x', 500),
-(504, 'ampolla Y', 500),
-(505, 'ampolla z', 600);
+INSERT INTO `producto` (`idProducto`, `nombre`, `precioVenta`, `unidades`) VALUES
+(100, 'Shampoo HyS', 500, 0),
+(200, 'Acondicionador Loreal', 600, 0),
+(300, 'Crema Pantene', 800, 0),
+(400, 'Ampolla Loreal', 1500, 0),
+(500, 'Gel Rolda', 1000, 0),
+(501, 'Ampolla Sol', 500, 0),
+(502, 'Ampolla X', 500, 0),
+(503, 'ampolla x', 500, 0),
+(504, 'ampolla Y', 500, 0),
+(505, 'ampolla z', 600, 0),
+(506, 'juan', 500, 0),
+(507, 'dasd', 213, 0);
 
 -- --------------------------------------------------------
 
@@ -219,8 +240,20 @@ CREATE TABLE IF NOT EXISTS `rol_persona` (
 INSERT INTO `rol_persona` (`idPersona`, `idRol`) VALUES
 ('444', '001'),
 ('111', '002'),
+('123456789', '002'),
 ('222', '002'),
+('234234', '002'),
+('321', '002'),
+('324', '002'),
 ('333', '002'),
+('3871283', '002'),
+('432421', '002'),
+('543231', '002'),
+('831283', '002'),
+('12345678', '003'),
+('1343241', '003'),
+('3123', '003'),
+('3213123', '003'),
 ('555', '003'),
 ('1065657930', '004'),
 ('1234', '004'),
@@ -243,7 +276,7 @@ CREATE TABLE IF NOT EXISTS `servicio` (
   `tiempo` int(11) NOT NULL,
   `precio` int(11) NOT NULL,
   PRIMARY KEY (`idServicio`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=568 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=570 ;
 
 --
 -- Volcado de datos para la tabla `servicio`
@@ -254,7 +287,9 @@ INSERT INTO `servicio` (`idServicio`, `nombre`, `tiempo`, `precio`) VALUES
 (234, 'Corte de Cabello', 30, 900),
 (345, 'Masaje Relajante', 60, 1500),
 (456, 'Manicure', 30, 900),
-(567, 'Pedicure', 40, 1000);
+(567, 'Pedicure', 40, 1000),
+(568, 'tratamiento capilar', 60, 70000),
+(569, 'tt', 40, 123);
 
 -- --------------------------------------------------------
 
@@ -290,8 +325,13 @@ CREATE TABLE IF NOT EXISTS `servicio_empleado` (
 
 INSERT INTO `servicio_empleado` (`idPersona`, `idServicio`) VALUES
 ('111', 123),
+('3871283', 123),
+('831283', 123),
 ('222', 234),
+('3871283', 234),
 ('333', 345),
+('3871283', 345),
+('831283', 345),
 ('111', 456),
 ('222', 567);
 
